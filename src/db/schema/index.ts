87f374d52c5ab9227e4e085fabc955c8581f6b98
kanta1207 <kan0712livepool@gmail.com>
2024-05-24 16:18:05 -0700

    refactor: barrel export schemas

---
+++ src/db/schema/index.ts
@@ -0,0 +1,6 @@
+export { categories } from '../schema';
+export { tasks } from '../schema';
+export { taskSessions } from '../schema';
+export { locations } from './locations';
+export { masterTaskPriorityLevels } from './master_task_priority_levels';
+export { masterTaskSizes } from './master_task_sizes';
