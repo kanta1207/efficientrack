66c71ef6fa53aeb503e3a0ee29fb5846bcb3a04c
kanta1207 <kan0712livepool@gmail.com>
2024-05-25 23:34:54 -0700

    fix: fix db import in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 
-import db from './db/drizzle';
+import { db } from './db';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
