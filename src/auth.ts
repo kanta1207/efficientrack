aae8fb9db435a357bdcaeff7eb0ffa72b6e3712e
kanta1207 <kan0712livepool@gmail.com>
2024-05-28 18:29:25 -0700

    feat: add google OAuth provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,18 @@
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth, { NextAuthConfig } from 'next-auth';
 import GitHub from 'next-auth/providers/GitHub';
+import Google from 'next-auth/providers/Google';
 
 import { db } from '@/db';
 
 export const authConfig = {
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
   adapter: DrizzleAdapter(db),
   callbacks: {
     async session({ session, user }) {
