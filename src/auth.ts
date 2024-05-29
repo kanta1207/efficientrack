d3203cb8d11ff8bcc73943d5b7cb32991dcdeb85
kanta1207 <kan0712livepool@gmail.com>
2024-05-28 17:33:52 -0700

    feat: configration to get userId

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,18 @@
-import type { NextAuthConfig } from 'next-auth';
-import NextAuth from 'next-auth';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import NextAuth, { NextAuthConfig } from 'next-auth';
 import GitHub from 'next-auth/providers/GitHub';
+
+import { db } from '@/db';
 
 export const authConfig = {
   providers: [GitHub],
+  adapter: DrizzleAdapter(db),
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signOut } = NextAuth(authConfig);
