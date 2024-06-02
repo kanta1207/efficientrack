121f23142fb4f2c44d31afc7ec6ecaf748089796
kanta1207 <kan0712livepool@gmail.com>
2024-06-01 17:14:14 -0700

    refactor: remove unnecessary pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from 'next/navigation';
 
 import { auth } from '@/auth';
 
-export default async function ProfilePage() {
+export default async function Home() {
   const session = await auth();
 
   if (!session?.user) {
